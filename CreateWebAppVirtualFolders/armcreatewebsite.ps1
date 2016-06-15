43b284e694e77d3f22fdb20759c9e156872d7d9d
Juan Manuel Servera <jmservera@outlook.com>
2016-06-15 16:34:41 +0200

    virtual folders in web app

---
+++ CreateWebAppVirtualFolders/armcreatewebsite.ps1
@@ -0,0 +1,64 @@
+﻿$props = @{
+  "virtualApplications" = @(
+    @{
+      "virtualPath" = "/";
+      "physicalPath"= "site\wwwroot";
+    },
+    @{
+      "virtualPath" = "/foo";
+      "physicalPath"= "site\bar";
+    },
+        @{
+      "virtualPath" = "/what";
+      "physicalPath"= "site\wwwroot\whatever";
+    },
+        @{
+      "virtualPath" = "/bar";
+      "physicalPath"= "site\wwwroot\bar";
+    }
+  )
+}
+
+$apiVersion="2015-08-01"
+
+$resourceGroupName= "migrationlabs3"
+$siteName="jmserverademo003"
+$servicePlan="test3"
+$location="northeurope"
+
+Remove-Variable siteNotExists
+Remove-Variable rgNotExists
+Remove-Variable spNotExists
+
+
+# Login-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionId  f19b7695-9685-461e-9700-2d9674197593
+
+Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorVariable rgNotExists -ErrorAction Continue
+if($rgNotExists)
+{
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+}
+
+Get-AzureRmResource -ResourceGroupName $resourceGroupName -ResourceType Microsoft.Web/serverfarms -ResourceName $servicePlan -ApiVersion $apiVersion -ErrorVariable spNotExists -ErrorAction Continue
+
+if($spNotExists)
+{
+    New-AzureRmAppServicePlan -Name $servicePlan -Location $location -ResourceGroupName $resourceGroupName `
+    -Tier Standard -WorkerSize Small -NumberofWorkers 1
+}
+
+Get-AzureRmResource -ResourceGroupName $resourceGroupName -ResourceType Microsoft.Web/sites -ResourceName $siteName -ApiVersion $apiVersion -ErrorVariable siteNotExists -ErrorAction Continue
+
+if($siteNotExists)
+{
+    New-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $siteName -AppServicePlan $servicePlan -Location $location
+}
+
+
+Set-AzureRmResource -ResourceGroupName $resourceGroupName `
+ -ResourceType Microsoft.Web/sites/config `
+  -ResourceName "$siteName/web" `
+   -PropertyObject $props `
+   -ApiVersion 2015-08-01 -Force
