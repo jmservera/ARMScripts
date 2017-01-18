b38de586f979a55e20e989a53f1b7a2b0885c6aa
Juan Manuel Servera <jmservera@outlook.com>
2017-01-18 15:05:17 +0100

    fixed commas in script

--- CustomScaleSet/CustomScaleSet/CustomScripts/configScript.sh
+++ CustomScaleSet/CustomScaleSet/CustomScripts/configScript.sh
@@ -7,19 +7,21 @@ set -e
 echo "Welcome to configuressl.sh"
 echo "Number of parameters was: " $#
 
-if [ $# -ne 3 ]; then
-    echo usage: $0 {sasuri}      
-	exit 1
+if [ $# -ne 1 ]; then
+    echo usage: $0 {sasuri}
+        exit 1
 fi
+
+echo "url" $1
 
 update_nodeapp(){
 
-curl $1 /nodeserver
+curl -o /nodeserver/hello.js $1
 
 }
 
 
-update_nodeapp
+update_nodeapp $1
 
 
 echo "restarting node"
